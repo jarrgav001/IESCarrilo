ec74aa9477c6bdfa9f13555dfa374aa69a1825b9
jarrgav001 <116264851+jarrgav001@users.noreply.github.com>
2022-12-02 17:44:00 +0100

    Add files via upload

---
+++ fibonacciMain/src/main/java/es/iescarrillo/daw/endes/fibonacciMain/App2.java
@@ -0,0 +1,11 @@
+package es.iescarrillo.daw.endes.fibonacciMain;
+
+import es.iescarrillo.daw.endes.fibonacci.*;
+
+public class App2 
+{
+    public static void main( String[] args )
+    {
+        App.main(null);
+    }
+}
